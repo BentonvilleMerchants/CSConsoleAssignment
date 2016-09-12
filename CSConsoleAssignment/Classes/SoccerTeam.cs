ef2e53056fff7bcc8cb4724c3d0d42dc5109155e
Joe Isabell <joe@bentonvillemerchants.com>
2016-09-12 10:16:16 -0600

    Move classes from Main Program to seperate Class files

---
+++ CSConsoleAssignment/Classes/SoccerTeam.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSConsoleAssignment
+{
+    class SoccerTeam : Team
+    {
+        //SoccerTeam Class Attributes
+        public int draw;
+        public int goalsFor;
+        public int goalsAgainst;
+        public int differential;
+        public int points;
+
+        //Constructors
+        public SoccerTeam()
+        {
+
+        }
+
+        public SoccerTeam(string sTeamName, int iPoints)
+        {
+            this.name = sTeamName;
+            this.points = iPoints;
+        }
+    }
+}
--- CSConsoleAssignment/Program.cs
+++ CSConsoleAssignment/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+
 namespace CSConsoleAssignment
 {
     class Program
@@ -13,64 +16,59 @@ namespace CSConsoleAssignment
             //Declare Variables
             int iNumTeams;
             int iCount = 0;
-            string sTeamName; //NEED TO ASK: Should this variable go down in the while loop?
+            string sTeamName;
             int iTeamPoints;
 
-
-            Console.WriteLine("How many teams are in the tournament?");
-            string sUserInput = Console.ReadLine();
-
-            iNumTeams = ReadInt(sUserInput);
-
             List<SoccerTeam> stTeams = new List<SoccerTeam>();
-            //iNumTeams = 3;
-            while(iCount < iNumTeams)
+
+            Console.Write("How many teams are in the tournament? ");
+            iNumTeams = Convert.ToInt32(Console.ReadLine());
+
+
+            while (iCount < iNumTeams)
+                {
+                    try
+                    {
+                        Console.Write("\nEnter Team " + (iCount + 1) + "'s Name: ");
+                        sTeamName = UppercaseFirst(Console.ReadLine());
+
+                        Console.Write("Enter " + sTeamName + "'s points: ");
+                        iTeamPoints = Convert.ToInt32(Console.ReadLine());
+
+                        SoccerTeam stNewTeam = new SoccerTeam(sTeamName, iTeamPoints);
+                        stTeams.Add(stNewTeam);
+                        iCount++;
+                    }
+
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("\nPlease enter a number.");
+                    }
+
+                }
+
+
+            List<SoccerTeam> sortedTeams = stTeams.OrderByDescending(SoccerTeam => SoccerTeam.points).ToList();
+
+            Console.WriteLine("\n\nHere is the sorted list:");
+
+            Console.WriteLine("\nPosition           Name                       Points");
+            Console.Write("--------           ----                       ------");
+
+            iCount = 1;
+
+            foreach (SoccerTeam st in sortedTeams)
             {
-                
-                Console.WriteLine("\nEnter Team " + (iCount + 1) + "'s Name:");
-                //Console.ReadLine();
-                sTeamName = UppercaseFirst(Console.ReadLine());
+                Console.Write("\n" + Convert.ToString(iCount).PadRight(19, ' '));
+                Console.Write(Convert.ToString(st.name).PadRight(27, ' '));
+                Console.Write(Convert.ToString(st.points));
 
-                Console.WriteLine("\nEnter " + sTeamName + "'s points:");
-                sUserInput = Console.ReadLine();
-
-                iTeamPoints = ReadInt(sUserInput);
-
-
-                SoccerTeam stNewTeam = new SoccerTeam(sTeamName,iTeamPoints);
-                stTeams.Add(stNewTeam);
-
-                iCount= iCount + 1;
+                iCount++;
             }
 
-
+            Console.ReadLine();
         }
 
-        static int ReadInt(string sUserInput)
-        {
-            int iNum;
-            bool bCheck = true;
-
-            while(bCheck)
-            {
-
-
-                if (int.TryParse(sUserInput, out iNum)) // Try to parse the string as an integer
-                {
-                    
-                    bCheck = false; 
-                }
-                else
-                {
-                    Console.WriteLine("Please enter a valid number:");
-                    sUserInput = Console.ReadLine();
-                }
-            }
-
-            
-                
-            return iNum = int.Parse(sUserInput);
-        }
 
         static string UppercaseFirst(string s)
         {
@@ -82,40 +80,13 @@ namespace CSConsoleAssignment
             // Return char and concat substring.
             return char.ToUpper(s[0]) + s.Substring(1);
         }
-    }
 
-    class Team
-    {
-        public string name;
-        public int wins;
-        public int loss;
-    }
-
-    class SoccerTeam : Team
-    {
-        //SoccerTeam Class Attributes
-        public int draw;
-        public int goalsFor;
-        public int goalsAgainst;
-        public int differential;
-        public int points;
-
-        //Constructors
-        public SoccerTeam()
+        static int CheckInt(string sUserInput)
         {
 
-        }
 
-        public SoccerTeam(string sTeamName, int iPoints)
-        {
-            this.name = sTeamName;
-            this.points = iPoints;
+            return;
         }
-
     }
 
-    class Game
-    {
-
-    }
 }
